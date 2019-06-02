d3bc46612a49bdaf27bfe970c16f6bc4a60ae70f
atilganf <atilganyf@gmail.com>
2019-06-02 15:10:39 +0300

    slideShow completed

---
+++ main.js
@@ -0,0 +1,19 @@
+var currentSlide = 0;
+function showSlide(n){
+  var i;
+  var slides = document.getElementsByClassName('ss');
+  var dots = document.getElementsByClassName("dot");
+  if(n >= slides.length){n = 0};
+  if(n < 0){n = slides.length - 1; }
+  for(i = 0; i < slides.length; i++){
+    slides[i].style.display = "none";
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[n].style.display = "block";
+  dots[n].className += " active";
+  currentSlide = n;
+}
+function plusSlide(n){
+  currentSlide +=n;
+  showSlide(currentSlide);
+}
